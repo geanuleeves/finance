0e4e95ab1867bfc3fdc3c11560f5892075c9d210
pzl93 <pzl@youguwang.com.cn>
2018-07-25 16:31:16 +0800

    admin

--- datalayer/manage/src/main/java/com/waben/stock/datalayer/manage/service/AppVersionUpgradeService.java
+++ datalayer/manage/src/main/java/com/waben/stock/datalayer/manage/service/AppVersionUpgradeService.java
@@ -5,7 +5,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Date;
 
-import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
