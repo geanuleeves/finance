538bde7bb74cf3bc2615a8aa39686742e2f89f2a
chenk3299 <582294025@qq.com>
2018-07-30 22:52:06 +0800

    修改接口

--- applayer/tactics/src/main/java/com/waben/stock/applayer/tactics/controller/futures/FuturesContractOrderController.java
+++ applayer/tactics/src/main/java/com/waben/stock/applayer/tactics/controller/futures/FuturesContractOrderController.java
@@ -1,17 +1,21 @@
 package com.waben.stock.applayer.tactics.controller.futures;
 
 import com.waben.stock.applayer.tactics.business.futures.FuturesContractOrderBusiness;
+import com.waben.stock.applayer.tactics.security.SecurityUtil;
 import com.waben.stock.interfaces.dto.futures.FuturesContractOrderViewDto;
 import com.waben.stock.interfaces.pojo.Response;
 import com.waben.stock.interfaces.pojo.query.PageInfo;
 import com.waben.stock.interfaces.pojo.query.futures.FuturesContractOrderQuery;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -27,10 +31,21 @@ public class FuturesContractOrderController {
     @Autowired
     private FuturesContractOrderBusiness futuresContractOrderBusiness;
 
-    @GetMapping("/pages")
+    @PostMapping("/pages")
     @ApiOperation(value = "合约订单列表")
-    public Response<PageInfo<FuturesContractOrderViewDto>> pages(int page, int size) {
-        FuturesContractOrderQuery query = new FuturesContractOrderQuery(page, size);
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "contractId", value = "对应的合约ID", dataType = "long", paramType = "query", required = false),
+            @ApiImplicitParam(name = "commodityNo", value = "品种编号", dataType = "string", paramType = "query", required = false),
+            @ApiImplicitParam(name = "contractNo", value = "对应的合约ID", dataType = "string", paramType = "query", required = false)
+    })
+    public Response<PageInfo<FuturesContractOrderViewDto>> pages(@RequestParam(required = false) Long contractId,
+                                                                 @RequestParam(required = false)String commodityNo,
+                                                                 @RequestParam(required = false)String contractNo) {
+        FuturesContractOrderQuery query = new FuturesContractOrderQuery();
+        query.setPublisherId(SecurityUtil.getUserId());
+        query.setCommodityNo(commodityNo);
+        query.setContractId(contractId);
+        query.setContractNo(contractNo);
         return new Response<>(futuresContractOrderBusiness.pages(query));
     }
 
--- applayer/tactics/src/main/java/com/waben/stock/applayer/tactics/controller/futures/FuturesTradeEntrustController.java
+++ applayer/tactics/src/main/java/com/waben/stock/applayer/tactics/controller/futures/FuturesTradeEntrustController.java
@@ -1,18 +1,24 @@
 package com.waben.stock.applayer.tactics.controller.futures;
 
 import com.waben.stock.applayer.tactics.business.futures.FuturesTradeEntrustBusiness;
+import com.waben.stock.applayer.tactics.security.SecurityUtil;
 import com.waben.stock.interfaces.dto.futures.FuturesTradeEntrustDto;
 import com.waben.stock.interfaces.pojo.Response;
 import com.waben.stock.interfaces.pojo.query.PageInfo;
 import com.waben.stock.interfaces.pojo.query.futures.FuturesTradeEntrustQuery;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 /**
  * @author chenk 2018/7/27
@@ -27,10 +33,42 @@ public class FuturesTradeEntrustController {
     @Autowired
     private FuturesTradeEntrustBusiness futuresTradeEntrustBusiness;
 
-    @GetMapping("/pages")
+    @PostMapping("/pages")
     @ApiOperation(value = "交易委托列表")
-    public Response<PageInfo<FuturesTradeEntrustDto>> pages(int page, int size) {
-        FuturesTradeEntrustQuery query = new FuturesTradeEntrustQuery(page, size);
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "entrustNo", value = "委托编号", dataType = "string", paramType = "query", required = false),
+            @ApiImplicitParam(name = "contractId", value = "对应的合约ID", dataType = "long", paramType = "query", required = false),
+            @ApiImplicitParam(name = "commodityNo", value = "品种编号", dataType = "string", paramType = "query", required = false),
+            @ApiImplicitParam(name = "contractNo", value = "合约编号", dataType = "string", paramType = "query", required = false),
+            @ApiImplicitParam(name = "orderType", value = "订单交易类型", dataType = "string", paramType = "query", required = false),
+            @ApiImplicitParam(name = "entrustTime", value = "委托时间", dataType = "date", paramType = "query", required = false),
+            @ApiImplicitParam(name = "priceType", value = "价格类型", dataType = "string", paramType = "query", required = false),
+            @ApiImplicitParam(name = "tradeActionType", value = "交易开平仓类型", dataType = "string", paramType = "query", required = false),
+            @ApiImplicitParam(name = "state", value = "委托状态", dataType = "string", paramType = "query", required = false),
+            @ApiImplicitParam(name = "tradeTime", value = "交易成功时间", dataType = "date", paramType = "query", required = false)
+    })
+    public Response<PageInfo<FuturesTradeEntrustDto>> pages(@RequestParam(required = false) String entrustNo,
+                                                            @RequestParam(required = false) Long contractId,
+                                                            @RequestParam(required = false) String commodityNo,
+                                                            @RequestParam(required = false) String contractNo,
+                                                            @RequestParam(required = false) String orderType,
+                                                            @RequestParam(required = false) Date entrustTime,
+                                                            @RequestParam(required = false) String priceType,
+                                                            @RequestParam(required = false) String tradeActionType,
+                                                            @RequestParam(required = false) String state,
+                                                            @RequestParam(required = false) Date tradeTime) {
+        FuturesTradeEntrustQuery query = new FuturesTradeEntrustQuery();
+        query.setPublisherId(SecurityUtil.getUserId());
+        query.setEntrustNo(entrustNo);
+        query.setContractId(contractId);
+        query.setCommodityNo(commodityNo);
+        query.setContractNo(contractNo);
+        query.setOrderType(orderType);
+        query.setEntrustTime(entrustTime);
+        query.setPriceType(priceType);
+        query.setTradeActionType(tradeActionType);
+        query.setState(state);
+        query.setTradeTime(tradeTime);
         return new Response<>(futuresTradeEntrustBusiness.pages(query));
     }
 
