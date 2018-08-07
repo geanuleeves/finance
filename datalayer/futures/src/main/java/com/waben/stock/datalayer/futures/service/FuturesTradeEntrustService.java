50e5ef4a036b4bc89ab90705e4cab152e6d78f97
493671072@qq.com <493671072@qq.com>
2018-08-07 20:47:59 +0800

    fix bug

--- datalayer/futures/src/main/java/com/waben/stock/datalayer/futures/service/FuturesTradeEntrustService.java
+++ datalayer/futures/src/main/java/com/waben/stock/datalayer/futures/service/FuturesTradeEntrustService.java
@@ -164,8 +164,7 @@ public class FuturesTradeEntrustService {
 							contractOrder.getBuyFallCanUnwindQuantity().add(action.getQuantity()));
 					contractOrderDao.doUpdate(contractOrder);
 				}
-				if (order.getCloseRemaining().compareTo(BigDecimal.ZERO) > 0
-						|| order.getCloseFilled().compareTo(BigDecimal.ZERO) > 0) {
+				if (order.getCloseFilled().compareTo(BigDecimal.ZERO) > 0) {
 					order.setState(FuturesOrderState.PartUnwind);
 				} else {
 					order.setState(FuturesOrderState.Position);
@@ -486,7 +485,7 @@ public class FuturesTradeEntrustService {
 
 					}
 					// 计算开仓的均价
-					BigDecimal openAvgFillPrice = totalOpenCost.multiply(totalUnwindQuantity);
+					BigDecimal openAvgFillPrice = totalOpenCost.divide(totalUnwindQuantity);
 					BigDecimal[] divideArr = openAvgFillPrice.divideAndRemainder(minWave);
 					openAvgFillPrice = divideArr[0].multiply(minWave);
 					entrust.setOpenAvgFillPrice(openAvgFillPrice);
